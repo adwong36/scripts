a9cb9564854743c28b5ae7e248cfbe96b4290a68
Adrian Wong <adrian.wong36@gmail.com>
2021-06-06 17:20:37 -0400

    zsh install

---
+++ zsh/zsh_setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+apt install zsh
+apt install direnv
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# updated ~/zshrc
+config=~/.zshrc
+if [ -f "$config" ]; then
+    echo "updating ~/.zshrc"
+    cp ./zshrc-sample ~/.zshrc
+fi
+
+install zsh addon
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
